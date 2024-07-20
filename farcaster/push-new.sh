a06f8a9267edbfeaa67d206a22fe5c1fc3bc79db
Silvan <cnsilvan@gmail.com>
2024-07-20 10:44:51 +0800

    Update push-new.sh

--- farcaster/push-new.sh
+++ farcaster/push-new.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 cd /root/hub-monorepo
+
 # 获取当前的Git标签
 current_tag=$(git describe --tags)
-# 获取最新的Git标签
-git fetch --tags && git checkout -- apps/hubble/src/cli.ts
-latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-# 比较最新标签和当前标签
-if [ "$latest_tag" != "$current_tag" ]; then
-  echo "Updating to the latest tag: $latest_tag"
+
+# 获取Docker Hub上的最新镜像标签
+docker pull cnsilvan/hubble:latest
+docker pull farcaster/hubble:latest
+
+cnsilvan_latest=$(docker inspect --format='{{index .RepoTags 0}}' cnsilvan/hubble:latest | awk -F: '{print $2}')
+farcaster_latest=$(docker inspect --format='{{index .RepoTags 0}}' farcaster/hubble:latest | awk -F: '{print $2}')
+
+# 比较Docker Hub镜像的最新标签
+if [ "$cnsilvan_latest" != "$farcaster_latest" ]; then
+  echo "Updating to the latest tag from farcaster: $farcaster_latest"
+  # 获取最新的Git标签
+  git fetch --tags && git checkout -- apps/hubble/src/cli.ts
+  latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
   # 更新到最新标签
   git checkout $latest_tag
   sed -i 's/if (totalMemory < 15) {/if (totalMemory < 3) {/' apps/hubble/src/cli.ts
@@ -15,15 +25,13 @@ if [ "$latest_tag" != "$current_tag" ]; then
   if yarn install && yarn build; then
     echo "Build successful, pushing to Docker..."
     docker buildx build -f Dockerfile.hubble \
-    --platform "linux/amd64" \
-    --push \
-    -t cnsilvan/hubble:${HUBBLE_VERSION} \
-    -t cnsilvan/hubble:latest .
+      --platform "linux/amd64" \
+      --push \
+      -t cnsilvan/hubble:${farcaster_latest} \
+      -t cnsilvan/hubble:latest .
   else
     echo "Build failed, not pushing to Docker."
   fi
-  
 else
-  echo "Already at the latest tag: $current_tag"
+  echo "cnsilvan/hubble:latest is already up-to-date with farcaster/hubble:latest"
 fi
-
